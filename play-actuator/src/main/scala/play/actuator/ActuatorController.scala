f79dd17220d2d90921577d91514fef1ffadac78f
Felipe Bonezi <felipebonezi@gmail.com>
2022-07-09 20:38:51 -0300

    feat(health): :sparkles: add health route
    Signed-off-by: Felipe Bonezi <felipebonezi@gmail.com>

---
+++ play-actuator/src/main/scala/play/actuator/ActuatorController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Felipe Bonezi <https://about.me/felipebonezi>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package play.actuator
+
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.BaseController
+import play.api.mvc.ControllerComponents
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class ActuatorController @Inject() (cc: ControllerComponents)(implicit
+    ec: ExecutionContext
+) extends BaseController {
+
+  def health: Action[AnyContent] = Action {
+    Ok(Json.obj("status" -> "UP"))
+  }
+
+  def info: Action[AnyContent] = TODO
+
+  protected override def controllerComponents: ControllerComponents = this.cc
+
+}
